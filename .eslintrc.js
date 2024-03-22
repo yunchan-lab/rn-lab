e6d4c66ee54d062905db2ca18baa878d0426ccdb
yunchanpark <yongp98@nate.com>
2024-03-22 18:34:06 +0900

    style: eslint, prettier 설정 및 ruby-version, node-version 명시

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,60 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+    "react-native/react-native": true,
+    // "jest/globals": true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "eslint:recommended",
+    // "plugin:jest/recommended",
+  ],
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+
+  plugins: ["react", "react-native"], // 'detox'],
+  ignorePatterns: ["!.*", "dist", "node_modules"],
+  rules: {
+    "no-mixed-spaces-and-tabs": 0, // disable rule
+    indent: "off",
+    // indent: [
+    //   "error",
+    //   "tab",
+    //   {
+    //     SwitchCase: 1,
+    //     ignoredNodes: ["ConditionalExpression"],
+    //   },
+    // ],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+    // "no-console": ["error"],
+    "no-unused-vars": "off", //['error', { vars: 'all' }],
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "no-undef": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    // "no-var-requires": "off",
+  },
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  singleQuote: false,
+  semi: false,
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+}
